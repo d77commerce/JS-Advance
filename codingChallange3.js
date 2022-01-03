d85bb1e8036f5042d39411d2eba0cdd98cf9c9ad
Danail Tsachev <73217857+d77commerce@users.noreply.github.com>
2022-01-03 23:01:20 +0000

    new

--- codingChallange3.js
+++ codingChallange3.js
@@ -2,12 +2,21 @@
 function calcAge1 (birthY){
     return 2022-birthY;
 }
+
 console.log(calcAge1(1977));
 //function expression
 const calcAge2 = function(birthY){
     return 2022-birthY;
 }
+
 console.log(calcAge2(1977));
 //Arrow function
 const calcAge3 = birthY => 2022-birthY;
 console.log(calcAge3(1977));
+
+const YearsToPention = (birthY,firstName)=>{
+    let age = 2022-birthY;
+    let pention = 65-age;
+    return `${firstName} retires  ${pention} years `;
+}
+console.log(YearsToPention(1977,'Petre'))
