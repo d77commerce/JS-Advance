37d31f2e1c62842e36d585d7af6354c5b51e0f99
Danail Tsachev <73217857+d77commerce@users.noreply.github.com>
2022-01-04 23:28:07 +0000

    new

--- arrays.js
+++ arrays.js
@@ -4,5 +4,8 @@ function testArrays (){
     const num  = new Array(1,2,3,4,9);
     console.log(num.length)
     console.log(num[2]);
+    const firstName = 'Elley';
+    const elley = [firstName,'Jhones',frend]
+    console.log(elley);
 }
 testArrays();
