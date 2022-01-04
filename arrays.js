a4019c9ba96f4bb907b141e188ecca04cfa3b695
Danail Tsachev <73217857+d77commerce@users.noreply.github.com>
2022-01-04 23:02:45 +0000

    new

---
+++ arrays.js
@@ -0,0 +1,8 @@
+function testArrays (){
+    const frend = ['Petre','ivan','Ana'];
+    console.log(frend[0]);
+    const num  = new Array(1,2,3,4,9);
+    console.log(num.length)
+    console.log(num[2]);
+}
+testArrays();
