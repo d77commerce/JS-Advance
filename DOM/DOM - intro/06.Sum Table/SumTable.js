1e1e5eef2dd6dc5a4df74cdf4f95a9ed16d06e30
Danail Tsachev <73217857+d77commerce@users.noreply.github.com>
2022-01-24 21:11:07 +0000

    new

--- DOM/DOM - intro/06.Sum Table/SumTable.js
+++ DOM/DOM - intro/06.Sum Table/SumTable.js
@@ -1,3 +1,13 @@
 function sumTable() {
+  let num = 0;
+  let total = document.getElementsByTagName("td");
+  const arrTotal = Array.from(total);
 
+  for (const x in arrTotal) {
+    if (x % 2 != 0) {
+      num += Number(arrTotal[x].textContent);
+    }
+  }
+  console.log(num);
+  document.getElementById("sum").textContent = num;
 }
