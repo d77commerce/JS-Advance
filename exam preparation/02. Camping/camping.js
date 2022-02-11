3fbcb3815d83931d934707722eaa25a15084350b
Danail Tsachev <73217857+d77commerce@users.noreply.github.com>
2022-02-11 16:58:31 +0000

    new

---
+++ exam preparation/02. Camping/camping.js
@@ -0,0 +1,11 @@
+function SummerCamp() {
+  class SummerCamp {
+    constructor(organizer, location) {
+      this.organizer = organizer;
+      this.location = location;
+      priceForTheCamp = { child: 150, student: 300, collegian: 500 };
+      listOfParticipants = [0];
+    }
+    registerParticipant(name, condition, money) {}
+  }
+}
