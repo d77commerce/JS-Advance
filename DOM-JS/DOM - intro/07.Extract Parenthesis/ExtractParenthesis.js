e320bc1bab94770a5a0039ddfb9185d14a5be5cf
Danail Tsachev <73217857+d77commerce@users.noreply.github.com>
2022-02-12 13:55:49 +0000

    new

--- DOM-JS/DOM - intro/07.Extract Parenthesis/ExtractParenthesis.js
+++ DOM-JS/DOM - intro/07.Extract Parenthesis/ExtractParenthesis.js
@@ -1,3 +1,13 @@
 function extract(content) {
-  console.log("2");
+  let result = document.getElementById(content).textContent;
+  //console.log(result);
+  let regex = RegExp(/\((.+?)\)/, "g");
+  let arr = result.match(regex);
+  for (let i = 0; i < arr.length; i++) {
+    while (arr[i].includes("(") || arr[i].includes(")")) {
+      arr[i] = arr[i].replace("(", "");
+      arr[i] = arr[i].replace(")", ";");
+    }
+  }
+  return arr.join(";");
 }
