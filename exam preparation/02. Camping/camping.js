ab5912b34c0ab9f44de23a8b8a6b08a82d6cf6ab
Danail Tsachev <73217857+d77commerce@users.noreply.github.com>
2022-02-12 09:08:14 +0000

    new

--- exam preparation/02. Camping/camping.js
+++ exam preparation/02. Camping/camping.js
@@ -1,11 +1,18 @@
-function SummerCamp() {
-  class SummerCamp {
-    constructor(organizer, location) {
-      this.organizer = organizer;
-      this.location = location;
-      priceForTheCamp = { child: 150, student: 300, collegian: 500 };
-      listOfParticipants = [0];
-    }
-    registerParticipant(name, condition, money) {}
+class SummerCamp {
+  constructor(organizer, location) {
+    this.organizer = organizer;
+    this.location = location;
+    this.priceForTheCamp = { child: 150, student: 300, collegian: 500 };
+    this.listOfParticipants = [0];
   }
+  registerParticipant(name, condition, money) {}
 }
+
+const summerCamp = new SummerCamp(
+  "Jane Austen",
+  "Pancharevo Sofia 1137, Bulgaria"
+);
+console.log(summerCamp.registerParticipant("Petar Petarson", "student", 200));
+console.log(summerCamp.registerParticipant("Petar Petarson", "student", 300));
+console.log(summerCamp.registerParticipant("Petar Petarson", "student", 300));
+console.log(summerCamp.registerParticipant("Leila Wolfe", "childd", 200));
