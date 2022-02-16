a55b1961b673ab1113a11864104d01d60876f478
Danail Tsachev <73217857+d77commerce@users.noreply.github.com>
2022-02-16 00:29:05 +0000

    new

--- DOM-JS/DOM - intro2/04. Search in List/search.js
+++ DOM-JS/DOM - intro2/04. Search in List/search.js
@@ -1,3 +1,21 @@
 function search() {
-   // TODO
+  let arrTowns = Array.from(
+    document.querySelector("#towns").querySelectorAll("li")
+  );
+  //console.log(...arrayInTowns);
+  arrTowns.forEach((e) => {
+    e.style.fontWeight = "normal";
+    e.style.textDecoration = "none";
+  });
+  let matches = 0;
+  let input = document.getElementById("searchText").value;
+  //console.log(input);
+  arrTowns.forEach((e) => {
+    if (e.textContent.includes(input)) {
+      e.style.fontWeight = "bolder";
+      e.style.textDecoration = "underline";
+      matches++;
+    }
+  });
+  document.getElementById("result").textContent = `${matches} matches found`;
 }
