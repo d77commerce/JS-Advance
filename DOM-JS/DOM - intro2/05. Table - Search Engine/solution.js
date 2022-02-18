01a971073fbe96ac619ea189dff176aa8c6cf092
Danail Tsachev <73217857+d77commerce@users.noreply.github.com>
2022-02-18 01:33:39 +0000

    new

--- DOM-JS/DOM - intro2/05. Table - Search Engine/solution.js
+++ DOM-JS/DOM - intro2/05. Table - Search Engine/solution.js
@@ -1,8 +1,17 @@
 function solve() {
-   document.querySelector('#searchBtn').addEventListener('click', onClick);
+  document.querySelector("#searchBtn").addEventListener("click", onClick);
 
-   function onClick() {
-      //   TODO:
+  function onClick() {
+    const btnSerch = document.getElementById("searchField").value.toLowerCase();
+    // console.log(btnSerch);
+    const arr = Array.from(
+      document.querySelector("tbody").querySelectorAll("tr")
+    );
 
-   }
+    arr.forEach((x) =>
+      x.textContent.toLowerCase().includes(btnSerch)
+        ? (x.className = "select")
+        : (x.className = "")
+    );
+  }
 }
