1b971735beed75677f146b5a0186c7b204c57a4e
Danail Tsachev <73217857+d77commerce@users.noreply.github.com>
2022-03-05 14:57:07 +0000

    new

--- JS-Applications/03. JS-Applications-Data-and-Authentication-Exercise-Resources/02.Phonebook/app.js
+++ JS-Applications/03. JS-Applications-Data-and-Authentication-Exercise-Resources/02.Phonebook/app.js
@@ -1,5 +1,29 @@
 function attachEvents() {
-    console.log('TODO...');
+  const url = `http://localhost:3030/jsonstore/phonebook`;
+
+  const phonebook = document.querySelector('#phonebook');
+
+  const btnLoad = document.querySelector('#btnLoad');
+  const btnCreate = document.querySelector('#btnCreate');
+
+  btnLoad.addEventListener('click', load);
+
+  async function load() {
+    let serverData = {};
+    const res = await fetch(url);
+    const data = await res.json();
+    Object.values(data).forEach(x => {
+      const { person, phone, _id } = x;
+      let html = `<li id="${_id}">${person}: ${phone} <button>DELETE</button></li>`;
+      // html.setAttribute(id, _id);
+      phonebook.insertAdjacentHTML('beforeend', html);
+
+      let li = document.createElement('li');
+      li.appendChild;
+      li.setAttribute('id', _id);
+    });
+    console.log(data);
+  }
 }
 
 attachEvents();
