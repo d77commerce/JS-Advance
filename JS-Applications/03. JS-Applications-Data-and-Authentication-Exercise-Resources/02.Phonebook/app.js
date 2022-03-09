cbcd7f288e57391c3dc2c8659b4efcf0504a8286
Danail Tsachev <73217857+d77commerce@users.noreply.github.com>
2022-03-09 23:26:29 +0000

    new

--- JS-Applications/03. JS-Applications-Data-and-Authentication-Exercise-Resources/02.Phonebook/app.js
+++ JS-Applications/03. JS-Applications-Data-and-Authentication-Exercise-Resources/02.Phonebook/app.js
@@ -3,26 +3,45 @@ function attachEvents() {
 
   const phonebook = document.querySelector('#phonebook');
 
+  const person = document.getElementById('person');
+  const phone = document.getElementById('phone');
+
   const btnLoad = document.querySelector('#btnLoad');
   const btnCreate = document.querySelector('#btnCreate');
 
   btnLoad.addEventListener('click', load);
+  btnCreate.addEventListener('click', create);
 
   async function load() {
-    let serverData = {};
+    phonebook.innerHTML = '';
     const res = await fetch(url);
     const data = await res.json();
     Object.values(data).forEach(x => {
       const { person, phone, _id } = x;
-      let html = `<li id="${_id}">${person}: ${phone} <button>DELETE</button></li>`;
-      // html.setAttribute(id, _id);
+      let html = `<li id="${_id}">${person}: ${phone} <button class="del">DELETE</button></li>`;
       phonebook.insertAdjacentHTML('beforeend', html);
 
-      let li = document.createElement('li');
-      li.appendChild;
-      li.setAttribute('id', _id);
+      // let li = document.createElement('li');
+      // phonebook.appendChild;
+      // li.setAttribute('id', _id);
+      const btnDel = document.querySelector('.del');
+      btnDel.addEventListener('click', console.log('del'));
     });
     console.log(data);
+  }
+  async function create() {
+    console.log('one');
+    if (person.value !== '' && phone.value !== '') {
+      const response = await fetch(url, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ person: person.value, phone: phone.value }),
+      });
+      load();
+
+      person.value = '';
+      phone.value = '';
+    }
   }
 }
 
