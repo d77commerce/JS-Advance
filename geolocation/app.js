0e7aff376011144219d9275d23ed35265c07987a
Danail Tsachev <73217857+d77commerce@users.noreply.github.com>
2022-03-10 10:13:35 +0000

    new

--- geolocation/app.js
+++ geolocation/app.js
@@ -1,20 +1,32 @@
 let btn = document.getElementById("btnGeo");
+const mapBtn = document.createElement("button");
+mapBtn.innerText = "SHOW in GOOGLE MAP";
+mapBtn.addEventListener("click", showInGoogleMap);
 let geolocationTextDisplay = document.getElementById("one");
 btn.addEventListener("click", test);
 let getLatitude = 0;
 let getLongitude = 0;
 
 // console.log("test");
-navigator.geolocation.getCurrentPosition(
-  function (position) {
-    console.log(position);
-    getLatitude = position.coords.latitude;
-    getLongitude = position.coords.longitude;
-  },
-  function () {
-    alert("Culd not get your position");
-  }
-);
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      console.log(position);
+      getLatitude = position.coords.latitude;
+      getLongitude = position.coords.longitude;
+    },
+    function () {
+      alert("Culd not get your position");
+    }
+  );
+}
+
 function test() {
   geolocationTextDisplay.innerText = `Your coordinats is ${getLatitude} , ${getLongitude}`;
+  const mapShowBtn = document.getElementById("geo").appendChild(mapBtn);
+}
+function showInGoogleMap() {
+  window.open(
+    `https://www.google.com/maps/@${getLatitude},${getLongitude},14z`
+  );
 }
