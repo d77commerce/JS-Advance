a365a9b2b65efc88c46e582031710d703748480e
Danail Tsachev <73217857+d77commerce@users.noreply.github.com>
2022-03-10 08:45:51 +0000

    new

--- geolocation/app.js
+++ geolocation/app.js
@@ -1,15 +1,24 @@
 let btn = document.getElementById("btnGeo");
+let geolocationTextDisplay = document.getElementById("one");
 btn.addEventListener("click", test);
+let getLatitude = 0;
+let getLongitude = 0;
 
-function test() {
-  console.log("test");
-  navigator.geolocation.getCurrentPosition(){
-      function(position){
-          
-      }
+// console.log("test");
+navigator.geolocation.getCurrentPosition(
+  function (position) {
+    console.log(position);
+    getLatitude = position.coords.latitude;
+    getLongitude = position.coords.longitude;
+  },
+  function () {
+    alert("Culd not get your position");
   }
+);
+function test() {
+  geolocationTextDisplay.innerText = `Your coordinats is ${getLatitude} , ${getLongitude}`;
 }
-test();
+
 document.getElementById("demo").addEventListener("click", myFunction);
 
 function myFunction() {
