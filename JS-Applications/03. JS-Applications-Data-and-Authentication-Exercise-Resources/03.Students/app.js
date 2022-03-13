b118f6bb419d08a07792df3860c6fe9266bba98b
Danail Tsachev <73217857+d77commerce@users.noreply.github.com>
2022-03-13 01:11:37 +0000

    new

--- JS-Applications/03. JS-Applications-Data-and-Authentication-Exercise-Resources/03.Students/app.js
+++ JS-Applications/03. JS-Applications-Data-and-Authentication-Exercise-Resources/03.Students/app.js
@@ -1,1 +1,17 @@
-console.log('TODO...');
+const url = " http://localhost:3030/jsonstore/collections/students";
+let studentsList = "";
+const table1 = document.querySelector("tbody");
+//table.appendChild("tbody");
+async function students() {
+  const res = await fetch(url);
+  const data = await res.json();
+  console.log(data);
+  Object.values(data).forEach((x) => {
+    const { firstName, lastName, facultyNumber, grade, _id } = x;
+    const thEl = `<th>${firstName}</th><th>${lastName}</th><th>${facultyNumber}</th><th>${grade}</th>`;
+    table1.insertAdjacentHTML("beforeend", thEl);
+    table1.style.textDecoration = "none";
+  });
+}
+
+students();
