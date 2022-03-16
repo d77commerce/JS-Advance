5c7b3c1a50bd998b47e9e32eb440bdf39e638b6f
Danail Tsachev <73217857+d77commerce@users.noreply.github.com>
2022-03-16 21:33:44 +0000

    new

--- JS-Applications/06. JS-Applications-Client-Side-Rendering-Exercise-Resources/01.List-Towns/app.js
+++ JS-Applications/06. JS-Applications-Client-Side-Rendering-Exercise-Resources/01.List-Towns/app.js
@@ -1,22 +1,25 @@
 import { html, render } from '../node_modules/lit-html/lit-html.js';
 
 let btn = document
-.querySelector('#btnLoadTowns')
-.addEventListener('click', townsLoad);
+  .querySelector('#btnLoadTowns')
+  .addEventListener('click', townsLoad);
 
-const templateList = (input)=>html`
-<ul>
-  ${input.map(town=>html`<li>${town}</li>`)}
-</ul>
-`
+const templateList = input => html`
+  <ul>
+    ${input.map(town => html`<li>${town}</li>`)}
+  </ul>
+`;
 
 function townsLoad(ev) {
   ev.preventDefault();
-  const townsIput = document.querySelector('#towns').value.split(', ');
-  const root = document.querySelector('#root');
+  if (document.querySelector('#towns').value !== '') {
+    const townsIput = document.querySelector('#towns').value.split(', ');
+    const root = document.querySelector('#root');
 
-  const 
-
+    const showList = templateList(townsIput);
+    render(showList, root);
+    document.querySelector('#towns').value = '';
+  }
 }
 /*
 let btn = document
