bfb57c11a0dd34b1dbb9f45b7be1255445675d6e
Danail Tsachev <73217857+d77commerce@users.noreply.github.com>
2022-03-20 21:14:46 +0000

    new

--- JS-Applications/03. JS-Applications-Data-and-Authentication-Exercise-Resources/04.Book-library/app.js
+++ JS-Applications/03. JS-Applications-Data-and-Authentication-Exercise-Resources/04.Book-library/app.js
@@ -1,1 +1,8 @@
-console.log('My requests...')
+const loadBooks = document.querySelector("#loadBooks");
+loadBooks.addEventListener("click", loadAllBooks);
+const urlBooks = "http://localhost:3030/jsonstore/collections/books";
+async function loadAllBooks() {
+  const res = await fetch(urlBooks);
+  const data = await res.json();
+  console.log(data);
+}
