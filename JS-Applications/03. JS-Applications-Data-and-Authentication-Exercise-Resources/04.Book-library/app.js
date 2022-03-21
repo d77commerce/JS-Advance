fd9904dfffbef17ca1b912a418836bc0d5728568
Danail Tsachev <73217857+d77commerce@users.noreply.github.com>
2022-03-21 00:13:11 +0000

    new

--- JS-Applications/03. JS-Applications-Data-and-Authentication-Exercise-Resources/04.Book-library/app.js
+++ JS-Applications/03. JS-Applications-Data-and-Authentication-Exercise-Resources/04.Book-library/app.js
@@ -1,8 +1,26 @@
+import { html, render } from "./node_modules/lit-html/lit-html.js";
+let data;
 const loadBooks = document.querySelector("#loadBooks");
 loadBooks.addEventListener("click", loadAllBooks);
 const urlBooks = "http://localhost:3030/jsonstore/collections/books";
-async function loadAllBooks() {
+const body = document.querySelector("#body");
+async function AllBooks() {
   const res = await fetch(urlBooks);
-  const data = await res.json();
+  data = await res.json();
+}
+AllBooks();
+const template = (input) => html` <tr>
+  ${input.array.forEach((element) => {
+    html`<td>${element.title}</td>
+      <td>${element}</td>
+      <td>
+        <button>Edit</button>
+        <button>Delete</button>
+      </td> `;
+  })}
+</tr>`;
+function loadAllBooks() {
+  const bookRend = template(data);
+  render(bookRend, body);
   console.log(data);
 }
