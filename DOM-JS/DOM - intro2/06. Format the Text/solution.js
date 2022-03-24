b339dac607bd4177a6b4053c9275ca69fd8a6e84
Danail Tsachev <73217857+d77commerce@users.noreply.github.com>
2022-03-24 10:07:31 +0000

    new

--- DOM-JS/DOM - intro2/06. Format the Text/solution.js
+++ DOM-JS/DOM - intro2/06. Format the Text/solution.js
@@ -1,3 +1,10 @@
 function solve() {
-  //TODO
+  const inputText = document.querySelector("#input");
+  const autput = document.querySelector("#output");
+  console.log(inputText.value);
+  inputText.value.split(".").forEach((element) => {
+    const pEl = document.createElement("p");
+    pEl.textContent = element;
+    autput.appendChild(pEl);
+  });
 }
