fb8ef3da2e1a51f2d87c30075d133a3d8e039d21
Danail Tsachev <73217857+d77commerce@users.noreply.github.com>
2022-03-26 21:12:02 +0000

    refactoring

--- JS-Applications/03. JS-Applications-Data-and-Authentication-Exercise-Resources/03.Students/app.js
+++ JS-Applications/03. JS-Applications-Data-and-Authentication-Exercise-Resources/03.Students/app.js
@@ -1,17 +1,47 @@
 const url = " http://localhost:3030/jsonstore/collections/students";
-let studentsList = "";
+
+const firstName = document.querySelector('input[name="firstName"]');
+const lastName = document.querySelector('input[name="lastName"]');
+const grade = document.querySelector('input[name="grade"]');
+const facultyNumber = document.querySelector('input[name="facultyNumber"]');
+
 const table1 = document.querySelector("tbody");
-//table.appendChild("tbody");
+
 async function students() {
   const res = await fetch(url);
   const data = await res.json();
   console.log(data);
   Object.values(data).forEach((x) => {
-    const { firstName, lastName, facultyNumber, grade, _id } = x;
+    const { firstName, lastName, facultyNumber, grade } = x;
     const thEl = `<th>${firstName}</th><th>${lastName}</th><th>${facultyNumber}</th><th>${grade}</th>`;
     table1.insertAdjacentHTML("beforeend", thEl);
     table1.style.textDecoration = "none";
   });
+}
+const addBtn = document.querySelector("#submit");
+addBtn.addEventListener("click", addStudent);
+async function addStudent(ev) {
+  ev.preventDefault();
+  const res = await fetch(url, {
+    method: "post",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      firstName: firstName.value,
+      lastName: lastName.value,
+      facultyNumber: facultyNumber.value,
+      grade: grade.value,
+    }),
+  });
+
+  // console.log(facultyNumber.value);
+  // console.log(firstName.value);
+  // console.log(lastName.value);
+  // console.log(grade.value);
+  // firstName.value = "";
+  lastName.value = "";
+  facultyNumber.value = "";
+  grade.value = "";
+  students();
 }
 
 students();
