1860cbe3e9f26a6d4989df721e8a99f18207b24d
Danail Tsachev <73217857+d77commerce@users.noreply.github.com>
2022-04-13 16:20:17 +0100

    new

--- JS-Applications/06. JS-Applications-Client-Side-Rendering-Exercise-Resources/03.Search-in-List/search.js
+++ JS-Applications/06. JS-Applications-Client-Side-Rendering-Exercise-Resources/03.Search-in-List/search.js
@@ -1,6 +1,25 @@
 import { html, render } from '../node_modules/lit-html/lit-html.js';
 import { towns } from './towns.js';
-console.log(towns[0]);
-towns.forEach(x => console.log(x.length));
+const townsDisplay = document.querySelector('#towns');
+const result = document.querySelector('#result');
+const btn = document.querySelector('#one');
+const input = document.querySelector('#searchText');
+btn.addEventListener('click', showResult);
+const templete = t => html`
+  <ul>
+    ${t.map(town => html`<li>${town}</li>`)}
+  </ul>
+`;
+
 search();
-function search() {}
+function search() {
+  render(templete(towns), townsDisplay);
+}
+function showResult() {
+  // input.preventdefault();
+  console.log(input.value);
+  for (let index = 0; index < input.value.length; index++) {
+    console.log('one');
+    console.log(input.value[index]);
+  }
+}
